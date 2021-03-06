fa1acb9d43a909898917a367681afa8d4f1f0e96
minpeter <kali2005611@gmail.com>
2021-03-06 19:25:21 +0900

    한세고 날씨 정보 기능 추가

--- weatherBOT.py
+++ weatherBOT.py
@@ -4,11 +4,11 @@ import requests
 import json
 
 client = discord.Client()
-discordbottoken="Nzk1NTc1MTA5ODU4ODg1NjUy.X_LXDw.L1gNTHfnkz_Jyv3wXjPvae36eSw"
+discordbottoken="here"
+openweatherapitoken="and here"
 
 def weatherrequests(lat, lon):
 
-    openweatherapitoken="f274a5616d927108ae79c369991175e5"
     response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={openweatherapitoken}&units=metric')
     rt = response.text
     json_data = json.loads(rt)
@@ -26,25 +26,29 @@ async def on_ready():
     print(client.user.id)
     print(client.user.name)
     print("ready")
-    await client.change_presence(status=discord.Status.online, activity = discord.Game("!김포날씨"))
+    await client.change_presence(status=discord.Status.online, activity = discord.Game("!help or !한세날씨"))
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith("!status"):
-        f = open('status.txt','r')
+        f = open("status.txt", "r", encoding="utf8")
         data = f.read()
         f.close()
         await message.channel.send(data)
 
     if message.content.startswith("!help"):
-        f = open('help.txt', 'r')
+        f = open("help.txt", "r",  encoding="utf8")
         data = f.read()
         f.close()
         await message.channel.send(data)
 
-    if message.content.startswith("!김포날씨"):
-        weather = weatherrequests(37,126)
-        await message.channel.send(f"```Mainweather : {weather[0]}\n온도 : {weather[1]}\n체감온도 : {weather[2]}\n습도 : {weather[3]}\n풍속 : {weather[4]}\n```")
+    if message.content.startswith("!김포날씨") or message.content.startswith("!ㄱㅍㄴㅅ"):
+        weather = weatherrequests(37.64524,126.67132)
+        await message.channel.send(f"```오늘의 날씨 : {weather[0]}\n온도 : {weather[1]}\n체감온도 : {weather[2]}\n습도 : {weather[3]}\n풍속 : {weather[4]}\n```")
+
+    if message.content.startswith("!한세날씨") or  message.content.startswith("!ㅎㅅㄴㅅ"):
+        weather = weatherrequests(37.55157,126.95194)
+        await message.channel.send(f"```오늘의 날씨 : {weather[0]}\n온도 : {weather[1]}\n체감온도 : {weather[2]}\n습도 : {weather[3]}\n풍속 : {weather[4]}\n```")
 
 client.run(discordbottoken)
